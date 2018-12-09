730a00bbbe27f1f5349d247f55002e1e2fdbdced
Calum Towers <me@calumtowers.com>
2018-12-09 20:23:02 +0000

    Fix namespace error

--- app/Console/Commands/Members/Awards.php
+++ app/Console/Commands/Members/Awards.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Members;
 
-use Illuminate\Console\Command;
+use App\Console\Commands\Command;
 
 class Awards extends Command
 {
