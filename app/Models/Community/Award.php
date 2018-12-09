70fbfa193a6cf21c66a7706062634c1b7f8e9112
Calum Towers <me@calumtowers.com>
2018-12-09 14:59:20 +0000

    Community awards

---
+++ app/Models/Community/Award.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Community;
+
+use App\Models\Model;
+use App\Models\Mship\Account;
+
+/**
+ * App\Models\Mship\Account\Award
+ *
+ * @property int $id
+ * @property int $awardable_type
+ * @property int $awardable_id
+ * @property int $forum_id
+ */
+class Award extends Model
+{
+    protected $table = 'community_awards';
+
+    public function awardable()
+    {
+        return $this->morphTo();
+    }
+
+    protected function accounts()
+    {
+        return $this->belongsToMany(Account::class);
+    }
+}
