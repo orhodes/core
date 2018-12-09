ca402edc7c0c695e86428399d7ee93ae549ae431
Calum Towers <me@calumtowers.com>
2018-12-09 20:29:59 +0000

    Apply fixes from StyleCI

--- database/factories/Community/AwardFactory.php
+++ database/factories/Community/AwardFactory.php
@@ -6,6 +6,6 @@ $factory->define(\App\Models\Community\Award::class, function (Faker $faker) {
     return [
         'awardable_type' => 'App\Models\Smartcars\Flight',
         'awardable_id' => factory(\App\Models\Smartcars\Flight::class)->create()->id,
-        'forum_id' => $faker->numberBetween(1,50),
+        'forum_id' => $faker->numberBetween(1, 50),
     ];
 });
